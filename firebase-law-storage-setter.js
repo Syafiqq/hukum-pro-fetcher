026cda5a4098b6a21d5b63dffbb60294a2586d7a
msyafiqq <id.muhammad.syafiq@gmail.com>
2022-01-29 13:26:45 +0700

    register store v0

--- firebase-law-storage-setter.js
+++ firebase-law-storage-setter.js
@@ -2,6 +2,68 @@ const { v4: uuidv4 } = require('uuid');
 const { bucket } = require('./firebase-initializer.js')
 const path = require("path");
 const logger = require('./logger.js')
+const fs = require('fs');
+const tmp = require('tmp');
+
+const storeV0 = async (files, date, token, isDryRun) => {
+    let contentBody = {
+        code: 200,
+        message: "Accepted",
+        data: {
+            tag: [],
+            data: [],
+            datatag: [],
+            version: [
+                {
+                    version: 1,
+                    timestamp: date
+                }
+            ]
+        }
+    }
+    let dataBody = []
+
+    const filename = token
+    logger.logProcess('Begin', 'upload', `${filename}`, 'to firebase')
+
+    for (const file of files) {
+        let raw = fs.readFileSync(file)
+        let laws = JSON.parse(raw.toString())
+        for(let i = 0; i < laws.length; i++) {
+            let law = laws[i]
+            law['id'] = law['_id']
+            law['category'] = parseInt(law['category'])
+            dataBody.push(law)
+        }
+    }
+    contentBody.data.data = dataBody
+
+    const tempFile = tmp.fileSync({ postfix: '.json' });
+    fs.writeFileSync(tempFile.name, JSON.stringify(contentBody))
+
+    if (!isDryRun) {
+        await bucket.upload(tempFile.name, {
+            // Support for HTTP requests made with `Accept-Encoding: gzip`
+            destination: `${process.env.FIREBASE_STORAGE_LOCATION}/${filename}`,
+            gzip: true,
+            // By setting the option `destination`, you can change the name of the
+            // object you are uploading to a bucket.
+            metadata: {
+                // Enable long-lived HTTP caching headers
+                // Use only if the contents of the file will never change
+                // (If the contents will change, use cacheControl: 'no-cache')
+                contentType: 'application/json; charset=utf-8',
+                metadata: {
+                    firebaseStorageDownloadTokens: uuidv4(),
+                }
+            },
+            public: true,
+            validation: 'md5',
+        });
+    }
+    tempFile.removeCallback()
+    logger.logProcess('Finish', 'upload', `${filename}`, 'to firebase')
+}
 
 const store = async (file, isDryRun) => {
     const filename = path.basename(file)
@@ -31,5 +93,6 @@ const store = async (file, isDryRun) => {
 }
 
 module.exports = {
+    storeV0,
     store,
 }
