9c6a36ff9e20d9bff0eda8d538903a36e2ace68a
msyafiqq <id.muhammad.syafiq@gmail.com>
2022-01-29 12:02:55 +0700

    add law order

---
+++ law-order.js
@@ -0,0 +1,8 @@
+module.exports = [
+    { id: '1', order: 1, name: 'UU' },
+    { id: '2', order: 2, name: 'TAP MPR' },
+    { id: '3', order: 3, name: 'UU DARURAT'},
+    { id: '4', order: 4, name: 'PERPU' },
+    { id: '5', order: 5, name: 'PP' },
+    { id: '6', order: 6, name: 'PERPRES' },
+]
