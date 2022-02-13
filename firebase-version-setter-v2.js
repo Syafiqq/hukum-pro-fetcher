2bf44d5206527ae7ce97468e9da22d2aaed558a1
msyafiqq <id.muhammad.syafiq@gmail.com>
2022-02-13 10:12:40 +0700

    change to f2

--- firebase-version-setter-v2.js
+++ firebase-version-setter-v2.js
@@ -2,11 +2,15 @@ const firebase = require('firebase/app');
 require('firebase/database')
 const logger = require("./logger");
 const path = require('path')
+const fs = require("fs");
 
 const store = async ({date, orders, laws, token, isDryRun}) => {
     logger.logProcess('Begin', 'save', `version ${token}`, 'to firebase')
     const versionPath = 'versions_new'
     const newPath = `${versionPath}/v2/${token}`
+
+    let orderString = fs.readFileSync(orders)
+    let orderJson = JSON.parse(orderString.toString())
 
     if (!isDryRun) {
         await firebase.database()
@@ -16,7 +20,7 @@ const store = async ({date, orders, laws, token, isDryRun}) => {
                 timestamp: date,
                 detail: {
                     law_filenames: laws.map((f) => path.basename(f)),
-                    orders: path.basename(orders)
+                    orders: orderJson
                 }
             })
     }
